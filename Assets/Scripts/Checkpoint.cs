5f1c7fa87bdc7669df95d4d7a92c9c9549891358
Xabier <79087721+xabism97@users.noreply.github.com>
2022-05-23 15:30:03 +0200

    Renderer Checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-
+    
     bool activated = true;
+
+    [SerializeField]
+    Renderer rend;
+
+    [SerializeField]
+    Texture texture;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player" && activated)
         {
-            
             CheckpointLogic.Instance.SetCheckpoint(this);
+            rend.material.SetTexture("_MainTex", texture);
+            
             activated = false;
         }
     }
