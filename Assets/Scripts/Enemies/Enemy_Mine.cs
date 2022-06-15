68e4cf7cd29e4a0d01a6442fe7736ee56c7b57a6
inigolens <inigolens@opendeusto.es>
2022-06-15 23:23:09 +0200

    Mina no funcions

--- Assets/Scripts/Enemies/Enemy_Mine.cs
+++ Assets/Scripts/Enemies/Enemy_Mine.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Enemy_Mine : MonoBehaviour
 {
     private Color defaultColor;
     private Color newColor;
-    private Renderer render;
+    [SerializeField] private Renderer render;
     private float tiempoExplosion = 1f;
     private float temporaltiempo = 0;
     private int nbucle;
     private bool colorb = false;
     private bool empezar;
     private float tiempoEspera= 0.1f;
-
+    [SerializeField] private GameObject explosionObject;
+    [SerializeField] private Transform parent;
+    [SerializeField] private Vector3 collisionRadius;
+    [SerializeField] private AudioSource beep;
+    [SerializeField] private float explosionRadius;
+    private SphereCollider colliderThis;
     // Start is called before the first frame update
     void Start()
     {
         nbucle = 3;
-        defaultColor = gameObject.GetComponent<Renderer>().material.color;
-        newColor = Color.red;
-        render = gameObject.GetComponent<Renderer>();
+        //render = gameObject.GetComponent<Renderer>();
+        defaultColor = render.materials[1].color;
+        newColor = new Color(139, 0, 0);
+        colliderThis = GetComponent<SphereCollider>();
+
     }
 
     // Update is called once per frame
@@ -34,15 +42,13 @@ public class Enemy_Mine : MonoBehaviour
        }
         
     }
-    void DestroyMine()
-    {
-        Destroy(gameObject);
-    }
-    private void OnTriggerEnter(Collider other)
+   
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             //DestroyMine();
+            
             if (!empezar)
             {
                 colorb = true;
@@ -63,15 +69,50 @@ public class Enemy_Mine : MonoBehaviour
             
             temporaltiempo = 0;
             nbucle--;
-            render.material.color = newColor;
+            render.materials[1].color = newColor;
+            beep.Play();
             yield return new WaitForSeconds(tiempoEspera);
-            render.material.color = defaultColor;
+            render.materials[1].color = defaultColor;
             yield return new WaitForSeconds(tiempoEspera*2);
         }
+        Instantiate(explosionObject, parent.position, explosionObject.transform.rotation);
         
-        DestroyMine();
+        colliderThis.tag = "Damage";
+        colliderThis.radius = explosionRadius;
+        Destroy(gameObject, 0.2f);
         
 
 
     }
 }
+
+/*
+ * namespace Xami.Vehicles
+{
+    public class RocketShellExplosion : MonoBehaviour
+    {
+        [SerializeField] private GameObject explosionObject;
+        [SerializeField] private Transform parent;
+        [SerializeField] private Vector3 collisionRadius;
+        [SerializeField] private LayerMask collisionLayer;
+
+        private void Update()
+        {
+            if (!parent.GetComponent<RocketShell>().IsJumping)
+            {
+                Collider[] deathCollider = Physics.OverlapBox(transform.position, collisionRadius, transform.rotation, collisionLayer, QueryTriggerInteraction.Ignore);
+                foreach (Collider i in deathCollider)
+                {
+                    Instantiate(explosionObject, parent.position, explosionObject.transform.rotation);
+                    LevelManager.Instance.Respawn(1f);
+                    transform.root.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, collisionRadius);
+        }
+    }
+}*/
